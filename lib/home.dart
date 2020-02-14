614d11afb39bcb1ee33fab55beffe7894994d3cd
Santiago <snole7@gmail.com>
2020-02-14 00:17:12 -0500

    Presentacion carrusel peliculas

--- lib/home.dart
+++ lib/home.dart
@@ -1,53 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class CineHome extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Carousel',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Carousel Demo'),
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CineLoj',
+        style: TextStyle(color: Colors.white),),
+      ),
+      backgroundColor: Colors.black,
+      body: ImageCarousel(),
+    ) ;
+  }
+
+}
+
+//metodo para agregar el carrusel de las imagenes
+class ImageCarousel extends StatelessWidget{
+  //se puede agregar un withget a una variable
+  final carousel = Carousel(
+    boxFit: BoxFit.cover,
+    images: [
+      AssetImage('assets/imagen.jpg'),
+      AssetImage('assets/peli1.jpg'),
+      AssetImage('assets/peli2.jpg'),
+
+    ],
+    animationCurve: Curves.fastOutSlowIn,
+    animationDuration: Duration(milliseconds: 2000),
+  );
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    double screemHeight = MediaQuery.of(context).size.height;
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        height: screemHeight/2,
+        child: new ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Stack(
+            children: <Widget>[
+              carousel,
+              banner()
+            ],
+          )
         ),
-        body: _body(),
       ),
     );
   }
 
-  Widget _body (){
-    return Stack(
-        children: <Widget>[
-          new PageView(
-            children: [testBGCarousel()],
-          ),
-          new ListView(
-            children: [
-              new Text('This text displays on top of the carousel'),
-            ],
-          ),
-        ]
+}
+
+class banner extends StatefulWidget {
+  @override
+  _bannerState createState() => _bannerState();
+}
+
+class _bannerState extends State<banner> with SingleTickerProviderStateMixin{
+
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =AnimationController(
+      duration: Duration(milliseconds: 2000),
+      vsync: this,
     );
+    animation = Tween(begin: 0.0, end: 18.0).animate(controller)..addListener(()
+    {setState(() {
+
+    });
+    });
+    controller.forward();
   }
 
-  Widget testBGCarousel(){
-    return CarouselSlider(
-      height: 400.0,
-      items: [1,2,3,4,5].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                decoration: BoxDecoration(
-                    color: Colors.amber
-                ),
-                child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-            );
-          },
-        );
-      }).toList(),
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(top: 20.0, left: 20.0),
+        child: new Container(
+          decoration: BoxDecoration(
+            color: Colors.amber.withOpacity(0.5),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0)
+            ),
+          ),
+          padding: EdgeInsets.all(10.0),
+          child: Text('Las mejores peliculas en estreno',
+            style: TextStyle(
+                fontFamily: 'fonts/Fira_Sans',
+                fontSize: animation.value//18.0
+            ),),
+        )
     );
+
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
 
